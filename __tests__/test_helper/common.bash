ca5546d0c87b85647543d2bb14f5d85521c93b2e
Ewa Maślanka <ewa.maslanka@cdq.com>
2026-03-16 09:25:38 +0100

    fix(CDLD-60460): Remove unused input variable exports from common setup

--- __tests__/test_helper/common.bash
+++ __tests__/test_helper/common.bash
@@ -3,15 +3,4 @@
 common_setup() {
   DIR="$(cd "$(dirname "$BATS_TEST_FILENAME")" && pwd)"
   PROJECT_ROOT="$(cd "$DIR/.." && pwd)"
-
-  export INPUT_VERSION=""
-  export INPUT_APP_NAME=""
-  export INPUT_ENVIRONMENT=""
-  export INPUT_DEPLOYED_AT=""
-  export INPUT_DESCRIPTION=""
-  export INPUT_COMMIT_SHA=""
-  export INPUT_REPOSITORY_FULL_NAME=""
-  export INPUT_INCLUDED_COMMIT_SHAS=""
-  export INPUT_FILE_PATH_FILTER=""
-  export INPUT_AUTH_HEADER=""
 }
